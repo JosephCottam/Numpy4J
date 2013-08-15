e8c8cbbc6e8934003d5abfaae9efd068c442fda0
Joseph Cottam <jcottam@indiana.edu>
2013-08-15 13:38:18 -0400

    Breakout py runtime init from 'hello' call

--- Explorations/combined/combined.c
+++ Explorations/combined/combined.c
@@ -2,12 +2,21 @@
 #include <stdio.h>
 #include "./bin/HelloJNI.h"
 #include <Python.h>
- 
-JNIEXPORT void JNICALL Java_HelloJNI_sayHello(JNIEnv *env, jobject thisObj) {
-  Py_SetProgramName("blended");  /* optional but recommended */
+
+
+jint JNI_OnLoad(JavaVM *vm, void *reserved) {
+  Py_SetProgramName("numpy4J (Bridge)");  /* optional but recommended */
   Py_Initialize();
+  return JNI_VERSION_1_6; //TODO: 1.2 is "any JNI version"...might be able to use that instead
+}
+
+void JNI_OnUnload(JavaVM *vm, void *reserved) {
+  Py_Finalize();
+}
+
+
+JNIEXPORT void JNICALL Java_HelloJNI_sayHello(JNIEnv *env, jobject thisObj) {
   PyRun_SimpleString("from time import time,ctime\n"
       "print 'Today is',ctime(time())\n");
-  Py_Finalize();
    return;
 }
